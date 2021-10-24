f2ec2a60725088e31521f0cdd8ced44b2803270b
Simanta-Developer <85247063+Simanta-Developer@users.noreply.github.com>
2021-10-24 09:47:12 +0530

    Create Python program to calculate BMI.py

---
+++ Some python programming problems.md/basic problems.md/Python program to calculate BMI.py
@@ -0,0 +1,4 @@
+weight = float(input("Enter your weight in kg :"))
+height = float(input("Enter your height in metres :"))
+BMI = weight / (height * height) # formula for BMI = weight(in kg)/ (height(in m))^2
+print("BMI is : ",BMI)
