3e056370da66e31ea7b7d7ebb46d2675b3c07847
Simanta-Developer <85247063+Simanta-Developer@users.noreply.github.com>
2021-10-29 23:05:48 +0530

    Create Count uppercase lowercase alphabets and digits .py

---
+++ Some python programming problems.md/Strings.py/Count uppercase lowercase alphabets and digits .py
@@ -0,0 +1,15 @@
+line = input("enter line :")
+lowercount = uppercount = digitcount = alphacount = 0 #initializing all the count variables.
+for a in line :
+  if a.islower():
+    lowercount+=1
+  elif a.isupper(): 
+    uppercount+=1
+  elif a.isdigit():
+    digitcount+=1
+  if a.isalpha():
+    alphacount+=1
+print("Number of uppercase letters :", uppercount)
+print("Number of lowercase letters :", lowercount)
+print("Number of digits :", digitcount)
+print("Number of alphabets :", alphacount) 
